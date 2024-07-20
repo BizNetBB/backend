6226f3db3d91b5c2f33ea8cb19d69719bb0dba7a
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2024-07-20 19:23:28 +0200

    Add skeletro for the notification service

---
+++ src/notification-service/src/test/java/com/biznetbb/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biznetbb.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
